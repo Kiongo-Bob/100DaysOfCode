6fd21a156af6feda5a80cbfceafcb8aff2738ba8
Kiongo-Bob <njoguub@gmail.com>
2023-10-20 15:26:36 +0300

    Geussing game - Loops refresher

---
+++ Practice/GeussingGame.py
@@ -0,0 +1,17 @@
+import random as rd
+
+geussed_int = rd.randint(1,100)
+
+print(geussed_int)
+
+user_guess = int(input("Guess a number between 1 and 100: "))
+
+for i in range (3):
+    if geussed_int != user_guess:
+        miss_value = user_guess - geussed_int
+        print(f"Try again, you missed by {miss_value}")
+        user_guess = int(input("Guess a number between 1 and 100: "))
+
+    else:
+        print("Jin-yas!")
+        break
